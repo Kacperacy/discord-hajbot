4af52b82ea455723a0b7f1417f1bbd463f112746
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-02-09 16:39:41 +0100

    .gitignore fix

--- dist/Commands.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Commands = void 0;
-const Hello_1 = require("./commands/Hello");
-const YoHighScore_1 = require("./commands/YoHighScore");
-const YoStreak_1 = require("./commands/YoStreak");
-exports.Commands = [Hello_1.Hello, YoHighScore_1.YoHighScore, YoStreak_1.YoStreak];
