fd71ff19b13e6a6626d627f6913a9a349986fcc2
Hajdew <hubertzabraniak@gmail.com>
2024-03-08 12:18:53 +0100

    added more bunttons and it automaticly disables after 5 seconds

--- src/commands/Hobby.ts
+++ src/commands/Hobby.ts
@@ -4,14 +4,30 @@ import {
   ApplicationCommandType,
   ButtonBuilder,
   ButtonStyle,
+  Emoji,
+  Collector,
 } from "discord.js";
 import { Command } from "../Command";
+import interactionCreate from "../listeners/interactionCreate";
 
-// Dodawanie przycisku
+// Add new button
 const button = new ButtonBuilder()
   .setCustomId("open_case_button")
   .setLabel("Open Case")
   .setStyle(ButtonStyle.Primary);
+  
+// Add new button
+const button2 = new ButtonBuilder()
+  .setCustomId("close_button")
+  .setLabel("Close")
+  .setStyle(ButtonStyle.Primary);
+  
+// Add new button
+const button3 = new ButtonBuilder()
+  .setCustomId("duck_button")
+  .setLabel(":duck:")
+  .setStyle(ButtonStyle.Primary)
+  
 
 export const Hobby: Command = {
   name: "hobby",
@@ -25,10 +41,50 @@ export const Hobby: Command = {
       content,
       components: [
         {
-          type: 1, // Typ 1 to wiersz akcji
-          components: [button.toJSON()], // Dodajemy przycisk do wiersza akcji
+          type: 1, 
+          components: [button.toJSON()], 
+        },
+        {
+          type: 1, 
+          components: [button2.toJSON()], 
+        },
+        {
+          type: 1, 
+          components: [button3.toJSON()], 
         },
       ],
     });
+
+
+    // Inreactions with buttons
+    client.on('interactionCreate', (interaction) => {
+      if(interaction.isButton()) {
+        if(interaction.customId === 'close_button') {
+          interaction.reply('Zamykam');
+        } else {
+          interaction.reply('yo');
+        }
+      }
+    })
+
+    // Disable buttons after 5 seconds
+    setTimeout(() => {
+      interaction.followUp({
+        components: [
+          {
+            type: 1,
+            components: [button.setDisabled(true).toJSON()],
+          },
+          {
+            type: 1,
+            components: [button2.setDisabled(true).toJSON()],
+          },
+          {
+            type: 1,
+            components: [button3.setDisabled(true).toJSON()],
+          },
+        ],
+      });
+    }, 5000);
   },
 };
