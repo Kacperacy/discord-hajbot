5d499ba9f55f4ef61b90efc601e19ff4cf138727
Kacper Maciołek <kacper.maciolek00@gmail.com>
2024-03-11 23:23:13 +0100

    https added

--- src/commands/Short.ts
+++ src/commands/Short.ts
@@ -23,7 +23,7 @@ const Short: SlashCommand = {
       return;
     }
 
-    const response = await fetch(`http://kacperacy.ovh/shorten?link=${url}`);
+    const response = await fetch(`https://kacperacy.ovh/shorten?link=${url}`);
 
     const data = (await response.json()) as ShortenResponse;
 
