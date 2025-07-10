317b4134b2b223236709c705a558008bf6284cdd
Kacper Maciołek <kacper.maciolek00@gmail.com>
2025-07-10 16:52:35 +0200

    shoot command added

---
+++ src/commands/Shoot.ts
@@ -0,0 +1,72 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { SlashCommand } from "../types/SlashCommand";
+import { Logger } from "../util/Logger";
+
+const Shoot: SlashCommand = {
+  command: new SlashCommandBuilder()
+    .setName("shoot")
+    .addUserOption((option) =>
+      option
+        .setName("target")
+        .setDescription("Wybierz gracza do ruletki")
+        .setRequired(true),
+    )
+    .setDescription("Kręcisz ruletą!"),
+  run: async (interaction: ChatInputCommandInteraction) => {
+    const target = interaction.options.getUser("target");
+    const shooter = interaction.user;
+
+    if (!target) {
+      await interaction.reply({
+        flags: 1 << 6,
+        content: "Nie wybrano gracza!",
+      });
+      return;
+    }
+
+    const rand = Math.floor(Math.random() * 100) + 1;
+
+    if (rand <= 5) {
+      try {
+        interaction.guild?.members.fetch(target.id).then((member) => {
+          member.timeout(
+            60 * 1000,
+            `${shooter.displayName} cię ustrzelił beka`,
+          );
+        });
+      } catch (err) {
+        Logger.getInstance().error(`User timeout error: ${err}`);
+      }
+
+      await interaction.reply({
+        content: `${shooter.displayName} ustrzelił ${target.displayName}`,
+      });
+      return;
+    }
+
+    if (rand <= 15) {
+      try {
+        interaction.guild?.members.fetch(interaction.user.id).then((member) => {
+          member.timeout(
+            60 * 1000,
+            `Nie trafiłeś w ${target.displayName} beka`,
+          );
+        });
+      } catch (err) {
+        Logger.getInstance().error(`User timeout error: ${err}`);
+      }
+
+      await interaction.reply({
+        content: `${shooter.displayName} nie trafił w ${target.displayName} i sam został ustrzelony`,
+      });
+
+      return;
+    }
+
+    await interaction.reply({
+      content: `${shooter.displayName} strzelał w ${target.displayName} ale trafił w powietrze`,
+    });
+  },
+};
+
+export default Shoot;
